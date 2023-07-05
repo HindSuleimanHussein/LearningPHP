8f36ee99d37300053f354c446c0bcdd56edf418b
Hind Suleiman Hussein <118082388+HindSuleimanHussein@users.noreply.github.com>
2023-07-05 07:14:14 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,33 +1,25 @@
+
+<?php
+echo "I like pizza <br>";
+echo "its really good";
+// this is how we comment in php
+/*
+and here's 
+multiline
+*/
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
 </head>
 <body>
-
-<form action="index.php" method="get">
-  <label>username:</label>
-  <input type="text" name="username"><br>
-
-  <br>
-
-  <label>password:</label>
-  <input type="text" name="password"><br>
-
-  <br>
-  <input type="submit" value="Log in">
-</form>
-<br>
-
-
+    <br>
+    <button>order pizza</button>
 </body>
 </html>
-<?php
-   echo "{$_GET["username"]}<br>";
-   echo $_GET["password"]."<br>";
-   echo "Hello There";
-
-?>
