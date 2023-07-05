5066110ff552748f485abeda1d4aa8d6712cde4f
Hind Suleiman Hussein <118082388+HindSuleimanHussein@users.noreply.github.com>
2023-07-05 07:50:12 +0300

    Add files via upload

---
+++ index8.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <form action="index.php" method=post>
+    <label>Give a Radius:</label>
+    <input type="text" name="radius">
+    <br><br>
+    
+    <input type="submit" value="Calculate">
+    <br><br>
+
+  </form>
+
+</body>
+
+</html>
+<?php
+$radius=$_POST["radius"];
+$circumference= null;
+$area=null;
+$volume=null;
+
+$circumference=2* pi() *$radius;
+$circumference=round($circumference, 2);
+
+$area=pi()*pow($radius, 2);
+$area=round($area, 2);
+
+$volume= 4/3*pi()*pow($radius, 3);
+$volume=round($volume, 2);
+
+echo "Circumference = {$circumference}cm <br>";
+echo "Area = {$area}cm^2 <br>";
+echo "Volume= {$volume}cm^3 <br>";
+
+
+?>
