c21e50f32ebdc9f27c3144e5d74a328c010dbbc4
Hind Suleiman Hussein <118082388+HindSuleimanHussein@users.noreply.github.com>
2023-07-05 07:12:38 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,39 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+<body>
+
+<form action="index.php" method="get">
+  <label>username:</label>
+  <input type="text" name="username"><br>
+
+  <br>
+
+  <label>password:</label>
+  <input type="text" name="password"><br>
+
+  <br>
+  <input type="submit" value="Log in">
+</form>
+<br>
+
+
+</body>
+</html>
 <?php
-    $name = "Hind";
-    $food="sushi";
-    $email="fake123@gmail.com";
-
-    $age=20;
-    $users=2;
-    $quantity=3;
-
-    $gpa=2.3;
-    $price=5.66;
-    $tax_rate=5.1;
-
-    $employed=true;
-    $online=false;
-    $for_sale=true;
-
-    $total=null;
-
-    echo "Hello {$name}<br>";
-    echo "You like {$food}<br>";
-    echo "Your email is {$email}<br>";
-
-    echo "You are {$age} years old<br>";
-    echo "There are {$users} users online<br>";
-    echo "You would like to buy {$quantity} items<br>";
-
-    echo "Your gpa is: {$gpa}<br>";
-    echo "Your pizza is \${$price} <br>";
-    echo "The sales tax rate is: {$tax_rate}%<br>";
-    echo "Online status: {$online}<br>"; //this gives 1 if online=true;
-
-    echo "You have ordered {$quantity}x {$food}s<br>";
-    $total=$quantity*$price;
-    echo "Your total is: \${$total}";
-
-    
+   echo "{$_GET["username"]}<br>";
+   echo $_GET["password"]."<br>";
+   echo "Hello There";
 
 ?>
