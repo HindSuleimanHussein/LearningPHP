fc513b57edd99c6d2ffb6811a75d461dee15cb44
Hind Suleiman Hussein <118082388+HindSuleimanHussein@users.noreply.github.com>
2023-07-05 18:42:06 +0300

    Update index1.php

--- index10.php
+++ index10.php
@@ -1,19 +1,40 @@
 <?php
-  $snacks=false;
-  $dinner=false;
+    $name = "Bro Code";
+    $food="pizza";
+    $name = "Hind";
+    $food="sushi";
+    $email="fake123@gmail.com";
 
-  if($snacks && $dinner){
-    echo "You have snacks and dinner";
-  }
-  else if($snacks==false && $dinner){
-    echo "You have dinner";
-  }
+    $age=20;
+    $users=2;
+    $quantity=3;
+    $gpa=2.3;
+    $price=5.66;
+    $tax_rate=5.1;
+    $employed=true;
+    $online=false;
+    $for_sale=true;
 
-  else if($dinner==false && $snacks){
-    echo "You have snacks";
-  }
-  else{
-    echo "You're broke";
-  }
+    $total=null;
 
+    /*echo "Hello {$name}<br>";
+    echo "Hello {$name}<br>";
+    echo "You like {$food}<br>";
+    echo "Your email is {$email}<br>";
+
+    echo "You are {$age} years old<br>";
+    echo "There are {$users} users online<br>";
+    echo "You would like to buy {$quantity} items<br>";
+
+    echo "Your gpa is: {$gpa}<br>";
+    echo "Your pizza is \${$price} <br>";
+    echo "The sales tax rate is: {$tax_rate}%<br>";*/
+    //echo "Online status: {$online}<br>" ->this gives 1 if online=true;
+    echo "The sales tax rate is: {$tax_rate}%<br>";
+    echo "Online status: {$online}<br>"; //this gives 1 if online=true;
+
+    echo "You have ordered {$quantity}x {$food}s<br>";
+    $total=$quantity*$price;
+    echo "Your total is: \${$total}";
+    
 ?>
